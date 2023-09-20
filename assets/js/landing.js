e56ede8aeac06b34656085540689c24ea050ad70
Mahdieh Shavandi <mdiehsh@gmail.com>
2023-09-20 10:04:29 +0330

    remove: removed the previous stylings duo to unrecongnizable errors - add: started mobile-first and from styling the header - mapped the menu contents and articles contents with JS - added the menu icon with the ability to change shape and show or hide menu

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,16 +1,65 @@
 function mapNavigation() {
-  const headerItems = [
-    { label: "Home", link: "/" },
-    { label: "Blog", link: "/blog" },
-    { label: "About Us", link: "/aboutus" },
-    { label: "Contact Us", link: "/contactus" },
-  ];
+    const headerItems = [
+        {label: "Home", link: "/"},
+        {label: "Blog", link: "/blog"},
+        {label: "About Us", link: "/aboutus"},
+        {label: "Contact Us", link: "/contactus"},
+    ];
 
-  document.getElementById("nav-header").innerHTML = headerItems
-    .map(
-      (el) => `
-    <a href=${el.link}><li class="header-item">${el.label}</li></a>
+    document.getElementById("nav-header").innerHTML = headerItems
+        .map(
+            (el) => `
+    <li class="header-item" onclick=${el.link} >${el.label}</li>
     `
-    )
-    .join("");
+        )
+        .join("");
+}
+
+mapNavigation()
+
+function renderArticles() {
+    const articles = [
+        {
+            image: 'https://picsum.photos/200?1',
+            alt: "dummy image for blog posts",
+            title: 'Code Optimization Made Easy!'
+        },
+        {
+            image: 'https://picsum.photos/200?1',
+            alt: "dummy image for blog posts",
+            title: 'Code Optimization Made Easy!'
+        },
+        {
+            image: 'https://picsum.photos/200?1',
+            alt: "dummy image for blog posts",
+            title: 'Code Optimization Made Easy!'
+        },
+        {
+            image: 'https://picsum.photos/200?1',
+            alt: "dummy image for blog posts",
+            title: 'Code Optimization Made Easy!'
+        },
+        {
+            image: 'https://picsum.photos/200?1',
+            alt: "dummy image for blog posts",
+            title: 'Code Optimization Made Easy!'
+        },
+    ]
+
+    document.getElementById('blog-section').innerHTML = articles.map(el =>
+        `<article class="article-card">
+            <img
+                    src=${el.image}
+                    alt=${el.alt}
+            />
+            <h3>${el.title}</h3>
+        </article>`
+    ).join("")
+}
+
+renderArticles()
+
+function onMenuIconClick(x) {
+    x.classList.toggle("change")
+    document.getElementById('nav-header').classList.toggle('change')
 }
