fea31ba6e9793f79b07760e4cbb02a59cd8c5c98
Mahdieh Shavandi <mdiehsh@gmail.com>
2023-10-03 15:47:13 +0330

    chore: added language page - scroll to top button - about and article webpages edited

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -4,6 +4,7 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -11,6 +12,29 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2500);
+}
+
+// Scroll to top logic
+let scrollToTopButton = document.getElementById("scroll-to-top-btn");
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
